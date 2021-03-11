5f4fe39e84274a29b77e2bf0121ecedce15d5fcc
Lindwing <rui.lopesgouveia@heig-vd.ch>
2021-03-11 10:30:23 +0100

    basé sur l'exemple du professeur.

--- labo-02/src/main/java/com/mycompany/app/Main.java
+++ labo-02/src/main/java/com/mycompany/app/Main.java
@@ -4,18 +4,27 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
+@CommandLine.Command(name = "new", description = "new")
+class New implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("new");
+        return 1;
+    }
+}
 
-@Command(name = "Main", mixinStandardHelpOptions = true)
+
+@Command(name = "Main", description = "Laboratoire du cours de génie logicielle", subcommands = { New.class})
 class Main implements Callable<Integer> {
 
     @Override
-    public Integer call() {
-        System.out.println("Subcommand needed: 'new', 'clean', 'build' or 'serve'");
+    public Integer call() throws Exception {
+        CommandLine.usage(this, System.out);
         return 0;
     }
 
     public static void main(String... args) {
-        int rc = new CommandLine(new Main()).execute(args);
-        System.exit(rc);
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 }
