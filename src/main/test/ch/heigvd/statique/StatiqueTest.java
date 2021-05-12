e9f2cc8afef561c5ff2d56099724693b235429d2
Mario Tomic <mario-tomic@hotmail.com>
2021-05-13 00:50:54 +0200

    Changement rapide du fichier de tests avec un début de tests à effectuer. Ne sachant pas comment exécuter les tests, j'ai pas fait bien plus.  Changes to be committed: 	modified:   src/main/test/ch/heigvd/statique/StatiqueTest.java

--- src/main/test/ch/heigvd/statique/StatiqueTest.java
+++ src/main/test/ch/heigvd/statique/StatiqueTest.java
@@ -37,4 +37,50 @@ class StatiqueTest {
     assertTrue(output.toString().contains("init"));
   }
 
+
+  @Test
+  void version() throws Exception {
+
+    new CommandLine(new Statique()).execute("-version");
+    assertTrue(output.toString().contains("Version: 0.0.2"));
+  }
+
+  @Test
+  void clean() throws Exception {
+
+    new CommandLine(new Statique()).execute("-version");
+    assertTrue(output.toString().contains("Version: 0.0.2"));
+  }
+
+  @Test
+  // Ce test devrait juste récupérer le contenu d'un dossier fraichement crée, et vérifier si celui-ci contient les fichiers à créer
+  void init() throws Exception {
+    private final String path = "test";
+    new CommandLine(new Statique()).execute("init " + path);
+    assertTrue(output.toString().contains(""));
+  }
+
+  /*
+
+  Techniquement pas besoin d'un test unitaire, suffit juste d'exécuter la commande
+
+
+  @Test
+  void serve() throws Exception {
+
+    new CommandLine(new Statique()).execute("serve");
+    assertTrue(output.toString().contains(""));
+  }
+
+   */
+
+  @Test
+  // La même que init, mais pour les fichiers de build
+  void build() throws Exception {
+    private final String path = "test";
+    new CommandLine(new Statique()).execute("build " + path);
+    assertTrue(output.toString().contains(""));
+  }
+
+
 }
